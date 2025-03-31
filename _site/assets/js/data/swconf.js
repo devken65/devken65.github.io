5eb86d0fc4d69eb3f27b1cfff6ffb91bb1e93ab8
Dev_ken65 <71168366+Zamasu0605@users.noreply.github.com>
2025-03-31 18:22:34 +0900

    build: commitlint적용

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743385856',resources: [
+    cacheName: 'chirpy-1743412922',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
